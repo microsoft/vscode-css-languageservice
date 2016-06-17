d6048e02d1336c45c9990b15d88d146e384be6e8
Martin Aeschlimann <martinae@microsoft.com>
2016-06-17 09:03:26 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+var gulp = require('gulp');
+var tsb = require('gulp-tsb');
+var assign = require('object-assign');
+var rimraf = require('rimraf');
+var merge = require('merge-stream');
+
+var compilation = tsb.create(assign({ verbose: true }, require('./src/tsconfig.json').compilerOptions));
+var tsSources = 'src/**/*.ts'
+
+function compileTask() {
+	return merge(
+		gulp.src('src/data/**', { base: '.' }),
+		gulp.src(tsSources).pipe(compilation())
+	)
+	.pipe(gulp.dest('lib'));
+}
+
+gulp.task('clean-out', function(cb) { rimraf('lib', { maxBusyTries: 1 }, cb); });
+gulp.task('compile', ['clean-out'], compileTask);
+gulp.task('compile-without-clean', compileTask);
+gulp.task('watch', ['compile'], function() {
+	gulp.watch(tsSources, ['compile-without-clean']);
+});
