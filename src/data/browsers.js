ec84f3393d8fdcaf5836f5b5888b0689b75e18c5
Martin Aeschlimann <martinae@microsoft.com>
2017-07-04 09:45:27 +0800

    Add flow-root (for #29)

--- src/data/browsers.js
+++ src/data/browsers.js
@@ -2221,6 +2221,10 @@
 						"browsers": "O12.1"
 					},
 					{
+						"name": "flow-root",
+						"browsers": "C58,FF53"
+					},
+					{
 						"name": "grid",
 						"browsers": "FF52,C57,S10.1,O44"
 					},
@@ -11249,6 +11253,7 @@
 	"block": "The element generates a block-level box",
 	"flex": "The element generates a principal flex container box and establishes a flex formatting context.",
 	"flexbox": "The element lays out its contents using flow layout (block-and-inline layout). Standardized as 'flex'.",
+	"flow-root": "The element generates a block container box, and lays out its contents using flow layout.",
 	"grid": "The element generates a principal grid container box, and establishes a grid formatting context.",
 	"inline-block": "A block box, which itself is flowed as a single inline box, similar to a replaced element. The inside of an inline-block is formatted as a block box, and the box itself is formatted as an inline box.",
 	"inline-flex": "Inline-level flex container.",
