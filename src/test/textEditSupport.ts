7fa38f7a0c057e75ef84b802300c5e456a8fc69a
Martin Aeschlimann <martinae@microsoft.com>
2018-02-08 17:00:30 +0100

    stable edit sort

--- src/test/textEditSupport.ts
+++ src/test/textEditSupport.ts
@@ -9,13 +9,20 @@ import * as assert from 'assert';
 
 export function applyEdits(document: TextDocument, edits: TextEdit[]): string {
 	let text = document.getText();
-	let sortedEdits = edits.sort((a, b) => document.offsetAt(b.range.start) - document.offsetAt(a.range.start));
+	let sortedEdits = edits.sort((a, b) => {
+		let diff = b.range.start.line - a.range.start.line;
+		if (diff === 0) {
+			diff = b.range.start.character - a.range.start.character;
+			if (diff === 0 && b.newText.length + a.newText.length === 0) { // two inserts at the same location
+				diff = edits.indexOf(b) - edits.indexOf(a);
+			}
+		}
+		return diff;
+	});
 	let lastOffset = text.length;
 	sortedEdits.forEach(e => {
 		let startOffset = document.offsetAt(e.range.start);
 		let endOffset = document.offsetAt(e.range.end);
-		assert.ok(startOffset <= endOffset);
-		assert.ok(endOffset <= lastOffset);
 		text = text.substring(0, startOffset) + e.newText + text.substring(endOffset, text.length);
 		lastOffset = startOffset;
 	});
