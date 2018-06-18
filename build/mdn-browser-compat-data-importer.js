2e5424bd574ff88607ed5da11d9ba628267dce73
Pine Wu <octref@gmail.com>
2018-06-18 09:14:59 -0700

    Use 'all' for properties supported in all browsers

--- build/mdn-browser-compat-data-importer.js
+++ build/mdn-browser-compat-data-importer.js
@@ -9,28 +9,28 @@ function addBrowserCompatDataToProperties(atdirectives, pseudoclasses, pseudoele
   atdirectives.forEach(item => {
     if (bcd.css['at-rules'][item.name.slice(1)]) {
       const matchingBCDItem = bcd.css['at-rules'][item.name.slice(1)] 
-      addBCDBrowsers(item, matchingBCDItem)
+      addBCDToBrowsers(item, matchingBCDItem)
     }
   })
 
   pseudoclasses.forEach(item => {
     if (bcd.css.selectors[item.name.slice(1)]) {
       const matchingBCDItem = bcd.css.selectors[item.name.slice(1)]
-      addBCDBrowsers(item, matchingBCDItem)
+      addBCDToBrowsers(item, matchingBCDItem)
     }
   })
 
   pseudoelements.forEach(item => {
     if (bcd.css.selectors[item.name.slice(2)]) {
       const matchingBCDItem = bcd.css.selectors[item.name.slice(2)]
-      addBCDBrowsers(item, matchingBCDItem)
+      addBCDToBrowsers(item, matchingBCDItem)
     }
   })
 
   properties.forEach(item => {
     if (bcd.css.properties[item.name]) {
       const matchingBCDItem = bcd.css.properties[item.name]
-      addBCDBrowsers(item, matchingBCDItem)
+      addBCDToBrowsers(item, matchingBCDItem)
     }
   })
 }
@@ -44,7 +44,7 @@ const browserNames = {
 	O: 'Opera'
 }
 
-function addBCDBrowsers(item, matchingBCDItem) {
+function addBCDToBrowsers(item, matchingBCDItem) {
   const compatString = toCompatString(matchingBCDItem)
 
   if (compatString !== '') {
@@ -59,6 +59,10 @@ function addBCDBrowsers(item, matchingBCDItem) {
 }
 
 function toCompatString(bcdProperty) {
+  if (isSupportedInAllBrowsers(bcdProperty)) {
+    return 'all'
+  }
+
   let s = []
   Object.keys(browserNames).forEach((abbrev) => {
     if (bcdProperty.__compat && bcdProperty.__compat.support[browserNames[abbrev].toLowerCase()]) {
@@ -72,6 +76,19 @@ function toCompatString(bcdProperty) {
     }
   })
   return s.join(',')
+}
+
+function isSupportedInAllBrowsers(bcdProperty) {
+  return Object.keys(browserNames).every((abbrev) => {
+    if (bcdProperty.__compat && bcdProperty.__compat.support[browserNames[abbrev].toLowerCase()]) {
+      const browserSupport = bcdProperty.__compat.support[browserNames[abbrev].toLowerCase()]
+      if (browserSupport) {
+        return isSupported(browserSupport)
+      }
+    }
+
+    return false
+  })
 }
 
 /**
@@ -112,6 +129,27 @@ function supportToShortCompatString(support, browserAbbrev) {
   }
 
   return null
+}
+
+function isSupported(support) {
+  let version_added
+  if (Array.isArray(support) && support[0] && support[0].version_added) {
+    version_added = support[0].version_added
+  } else if (support.version_added) {
+    version_added = support.version_added
+  }
+
+  if (version_added) {
+    if (typeof(version_added) === 'boolean') {
+      return version_added
+    } else if (typeof(version_added) === 'string') {
+      if (typeof(parseInt(version_added)) === 'number') {
+        return true
+      }
+    }
+  }
+
+  return false
 }
 
 module.exports.addBrowserCompatDataToProperties = addBrowserCompatDataToProperties
