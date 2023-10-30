52c5458e5dc6d07229aee14442e3557b4716a0d2
Simon Stranks <sstranks87@gmail.com>
2023-10-30 18:52:27 +0000

    fix()

--- src/services/cssNavigation.ts
+++ src/services/cssNavigation.ts
@@ -552,7 +552,7 @@ export class CSSNavigation {
 			}
 		}
 
-    // Try resolving the reference from the language configuration alias settings.
+    // Try resolving the reference from the language configuration alias settings
     if (ref && !(await this.fileExists(ref))){
       const rootFolderUri = documentContext.resolveReference('/', documentUri);
       if (settings?.paths && rootFolderUri) {
