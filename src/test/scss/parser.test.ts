d81e5dbcc330a785fac281221bde7973db6bfa06
Martin Aeschlimann <martinae@microsoft.com>
2024-04-08 15:06:19 +0200

    fix formatting

--- src/test/scss/parser.test.ts
+++ src/test/scss/parser.test.ts
@@ -292,7 +292,7 @@ suite('SCSS - Parser', () => {
 		const parser = new SCSSParser();
 		assertNode(`@container (min-width: #{$minWidth}) { .scss-interpolation { line-height: 10cqh; } }`, parser, parser._parseStylesheet.bind(parser));
 		assertNode(`.item-icon { @container (max-height: 100px) { .item-icon { display: none;  } } }`, parser, parser._parseStylesheet.bind(parser));
-		assertNode(`:root { @container (max-height: 100px) { display: none;} }`,parser,parser._parseStylesheet.bind(parser));
+		assertNode(`:root { @container (max-height: 100px) { display: none;} }`, parser, parser._parseStylesheet.bind(parser));
 	});
 
 	test('@use', function () {
